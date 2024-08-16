551a183adc0108a7b28cf34a38d8f6712b73a377
hotuanhai <hotuanhai2412@gmail.com>
2024-08-17 00:08:24 +0700

    progressbar

---
+++ assets/songsList/songs.js
@@ -0,0 +1,65 @@
+const songs = [
+    {
+        name: `Duality`,
+        singer: `Slipknot`,
+        path: `./assets/music/duality.mp3`,
+        image:`./assets/img/duality.jpg`
+    },
+    {
+        name:`Opium of the people`,
+        singer: `Slipknot`,
+        path:`./assets/music/opiumOfThePeople.mp3`,
+        image:`./assets/img/opiumOfThePeople.png`
+    },
+    {
+        name:`Spit it out`,
+        singer: `Slipknot`,
+        path:`./assets/music/spitItOut.mp3`,
+        image:`./assets/img/spitItOut.png`
+    },
+    {
+        name: `People = shit`,
+        singer: `Slipknot`,
+        path:`./assets/music/people=shit.mp3`,
+        image:`./assets/img/people=shit.jpg`
+    },
+    {
+        name:`Eyeless`,
+        singer: `Slipknot`,
+        path:`./assets/music/eyeless.mp3`,
+        image:`./assets/img/eyeless.png`
+    },
+    {
+        name: `Custer`,
+        singer: `Slipknot`,
+        path:`./assets/music/custer.mp3`,
+        image:`./assets/img/custer.jpg`
+    },
+    {
+        name:`Wait and bleed`,
+        singer: `Slipknot`,
+        path:`./assets/music/waitAndBleed.mp3`,
+        image:`./assets/img/waitAndBleed.jpg`
+    },
+    {
+        name:`(Sic)`,
+        singer: `Slipknot`,
+        path:`./assets/music/sic.mp3`,
+        image:`./assets/img/sic.jpg`
+    },
+    {
+        name:`My plague`,
+        singer: `Slipknot`,
+        path:`./assets/music/myPlague.mp3`,
+        image:`./assets/img/myPlague.png`
+    },
+    {
+        name:`Disasterpieces`,
+        singer: `Slipknot`,
+        path:`./assets/music/disasterpieces.mp3`,
+        image:`./assets/img/disasterpieces.webp`
+    },
+    
+]
+
+export default songs;
---
+++ musicPlayer.js
@@ -0,0 +1,112 @@
+import songs from './assets/songsList/songs.js'
+
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const player = $('.player')
+const playlist = $('.playlist')
+const cd = $('.cd')
+const heading = $('header h2')
+const cdThumb = $('.cd-thumb')
+const audio = $('#audio')
+const playBtn = $('.btn-toggle-play')
+const progress = $('#progress')
+
+const app = {
+    curIndex: 0,
+    isPlaying: false,
+    isSeeking: false,
+    songs,
+    render: function(){
+        const htmls = this.songs.map(function(song){
+            return`
+                <div class="song">
+                    <div class="thumb" style="background-image: url('${song.image}')">
+                    </div>
+                    <div class="body">
+                        <h3 class="title">${song.name}</h3>
+                        <p class="author">${song.singer}</p>
+                    </div>
+                    <div class="option">
+                        <i class="fas fa-ellipsis-h"></i>
+                    </div>
+                </div>
+            `
+        })
+        playlist.innerHTML = htmls.join('')
+    },
+    defineProperties: function(){
+        Object.defineProperty(this,'curSong',{
+            get : function(){
+                return this.songs[this.curIndex]
+            }
+        })
+    },
+    handleEvents: function(){
+        const _this = this
+        //set width,height of cd img when scrolling
+        const cdWidth = cd.offsetWidth
+        document.onscroll = function(){
+            const scrollTop = window.scrollY || document.documentElement.scrollTop
+            const newCDWidth = cdWidth - scrollTop/3
+            cd.style.width = newCDWidth > 0 ? newCDWidth + 'px' : 0
+            cd.style.opacity = newCDWidth/cdWidth
+        }
+        // handle pause / play btn
+        playBtn.onclick = function(){
+            if(_this.isPlaying){            
+                audio.pause()
+            }else{           
+                audio.play()
+            }
+        }
+
+        //handle play
+        audio.onplay = function(){
+            _this.isPlaying = true
+            audio.volume = 0.1
+            player.classList.add(`playing`)
+        }
+        //handle pause
+        audio.onpause = function(){
+            _this.isPlaying = false
+            audio.volume = 0.1
+            player.classList.remove(`playing`)
+        }
+        // update the progress bar 
+        audio.ontimeupdate = function(){
+            if(!_this.isSeeking && audio.duration){
+                const progressPercent = Math.floor(audio.currentTime / audio.duration *100)
+                progress.value = progressPercent
+            }
+        }
+        //tua
+        //isSeeking to handle confict of oninput-onchange and ontimeupdate
+        progress.oninput = function(e) {
+            _this.isSeeking = true; 
+        }
+        progress.onchange = function(e) {
+            const seekTime = e.target.value / 100 * audio.duration;
+            audio.currentTime = seekTime;
+            _this.isSeeking = false; // 
+        }
+        
+        
+    },
+    loadCurSong: function(){
+        heading.textContent = this.curSong.name
+        cdThumb.style.backgroundImage = `url('${this.curSong.image}')`
+        audio.src = this.curSong.path
+    },
+    start: function(){
+        //dinh nghia cac thuoc tinh cho obj
+        this.defineProperties()
+
+        this.handleEvents()
+        
+        this.loadCurSong()
+        
+        this.render()
+    }
+}
+app.start()
